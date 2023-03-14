d9dd0248df48dc2611d87a1e4c06568c3f7094c9
Bernadette Pluempe <bernadette.pluempe-ext@persona.de>
2023-03-14 16:48:01 +0100

    Changed storage account name

--- Subscriptions/rg-connectivity-001/backend.tf
+++ Subscriptions/rg-connectivity-001/backend.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "azurerm" {
     resource_group_name     = "rg-terraform-state-001-betty"
-    storage_account_name    = "cloudbettyterraformstate"
+    storage_account_name    = "cloudterraformstate"
     container_name          = "tfstate"
     key                     = "GitHub-Terraform-rg-connectivity-001-betty"
   }
